c0ff70a4eb72927760e87b15c49e4aeee3ca7cd7
Yuhan X WIN <yuhanxiao97@gmail.com>
2021-01-13 12:08:50 -0800

    re-init, to do list

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
-import React from 'react';
-import SearchBar from './SearchBar/SearchBar';
-import Map from './Map/Map';
-import './App.css';
-
-function App() {
-  const [query, setQuery] = React.useState("858 Coleman Ave");
-
-  return (
-    <div className="App">
-      <SearchBar {...{query, setQuery}} />
-      <Map />
-    </div>
-  );
-};
+const App = () => {
+  return <>Hello World</>;
+}
 
 export default App;
---
+++ src/ToDoListItem.tsx
@@ -0,0 +1,3 @@
+export const TodoListItem = () => {
+  return <li>content</li>;
+};
